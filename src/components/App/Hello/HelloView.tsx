408fd0a70691ed11c0bb9c656850b8318b890704
John Tucker <jtucker@appirio.com>
2018-11-23 17:23:45 -0500

    Fix

--- src/components/App/Hello/HelloView.tsx
+++ src/components/App/Hello/HelloView.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { QueryResult } from 'react-apollo';
 import { HelloData } from '../Hello';
 
-const HelloView = ({ error, loading, data: { hello } }: QueryResult<HelloData>) => {
-  if (loading) { return <div>fetching</div>; }
-  if (error) { return <div>errored</div>; }
-  return (
-    <div>hello {hello}</div>
-  );
-};
+class HelloView extends PureComponent<QueryResult<HelloData>> {
+  public render() {
+    const { error, loading, data: { hello }} = this.props;
+    if (loading) { return <div>fetching</div>; }
+    if (error) { return <div>errored</div>; }
+    return (
+      <div>hello {hello}</div>
+    );
+  }
+}
 
 export default HelloView;
